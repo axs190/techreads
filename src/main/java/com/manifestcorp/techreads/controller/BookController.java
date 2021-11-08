f857d47491407938e17aec28902a7a8517ba0ed4
axs190 <aschneir@manifestcorp.com>
2021-11-08 16:57:14 -0500

    Added add command

--- src/main/java/com/manifestcorp/techreads/controller/BookController.java
+++ src/main/java/com/manifestcorp/techreads/controller/BookController.java
@@ -1,12 +1,18 @@
 package com.manifestcorp.techreads.controller;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
 import com.manifestcorp.techreads.model.Book;
 import com.manifestcorp.techreads.repository.BookRepository;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 @RequestMapping("/books")
@@ -21,5 +27,17 @@ public class BookController {
         mav.addObject("books", books);
         return mav;
     }
+
+    @RequestMapping("/add")
+    public String add(Model model){
+        model.addAttribute("bookForm", new Book());
+        return"add";
+    }
+    @RequestMapping(value = {"", "/"}, method=POST)
+            public RedirectView addBook(Book book) {
+                bookRepository.saveAndFlush(book);
+                return new RedirectView("books");
+
+            }
 
 }
