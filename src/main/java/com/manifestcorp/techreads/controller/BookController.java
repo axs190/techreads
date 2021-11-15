c3e4453e8571a9feada5662be9f34632f20f6a73
axs190 <aschneir@manifestcorp.com>
2021-11-15 11:03:35 -0500

    Added Remove Function as well as added author and rating

--- src/main/java/com/manifestcorp/techreads/controller/BookController.java
+++ src/main/java/com/manifestcorp/techreads/controller/BookController.java
@@ -2,10 +2,10 @@ package com.manifestcorp.techreads.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -13,6 +13,8 @@ import com.manifestcorp.techreads.model.Book;
 import com.manifestcorp.techreads.repository.BookRepository;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+
 
 @Controller
 @RequestMapping("/books")
@@ -34,10 +36,16 @@ public class BookController {
         return"add";
     }
     @RequestMapping(value = {"", "/"}, method=POST)
-            public RedirectView addBook(Book book) {
-                bookRepository.saveAndFlush(book);
-                return new RedirectView("books");
+    public RedirectView addBook(Book book) {
+        bookRepository.saveAndFlush(book);
+        return new RedirectView("/books");
+    }
 
-            }
+    @GetMapping(value = {"/delete/{id}"})
+    public RedirectView removeBook(@PathVariable("id")Long id) {
+        bookRepository.deleteById(id);
+        return new RedirectView("/books");
+    }
+
 
 }
--- src/main/java/com/manifestcorp/techreads/model/Book.java
+++ src/main/java/com/manifestcorp/techreads/model/Book.java
@@ -11,14 +11,26 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
     String title;
+    String author;
+    String rating;
+    String cover;
     public Book() {}
 
     public Book(String title) {
+        this.id=id;
         this.title = title;
+        this.author= author;
+        this.rating= rating;
+
+
     }
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+    public String getRating() { return rating; }
+    public void setRating(String rating) { this.rating = rating; }
 
 }
